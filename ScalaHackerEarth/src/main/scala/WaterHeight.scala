cb24e8cbc11e7144eee8258a03e5fbe3834e5596
Eric Leon <ericleon727@gmail.com>
2025-11-21 12:47:02 +0000

    Implemented water height

--- ScalaHackerEarth/src/main/scala/WaterHeight.scala
+++ ScalaHackerEarth/src/main/scala/WaterHeight.scala
@@ -4,8 +4,23 @@ object WaterHeight {
   }
 
   def waterHeight(heights:List[Int]):Int = {
-
-
-    0
+    var volumes:List[Int] = List()
+    var prev = 0
+    for(i <- 0 until heights.length - 1){
+      val base = heights(i)
+      var volume = 0
+      val j = i + 1
+      heights.drop(j).foreach(h => {
+        if(base < h){
+          volume = base * (j - prev)
+        }
+        else{
+          volume = h * (j - prev)
+        }
+      })
+      volumes = volumes :+ volume
+      prev += 1
+    }
+    volumes.max
   }
 }
